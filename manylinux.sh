341d39c9d3fab85def9fec8ffe7c66af48322875
duhenglucky <duheng0522@gmail.com>
2019-10-28 18:04:54 +0800

    charo(ci) remove docker base image dependency

--- manylinux.sh
+++ manylinux.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-cp /usr/local/lib/librocketmq.so /io/rocketmq/librocketmq.so
-
 # Build wheels
-which linux32 && LINUX32=linux32
-$LINUX32 /opt/python/cp27-cp27mu/bin/python setup.py bdist_wheel
+/opt/python/cp27-cp27mu/bin/python setup.py bdist_wheel
 
 # Audit wheels
 for wheel in dist/*-linux_*.whl; do
